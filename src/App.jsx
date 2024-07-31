18a8af5602fad67c26dd1921109354d59c0728b9
lintu.gogi <lintu.joseph06@gmail.com>
2024-07-30 23:48:26 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import EmployeePage from './EmployeePage'
+import HomePage from './HomePage'
+
+function App() {
+  
+
+  return (
+    <>
+      <div style={{display: "flex", backgroundColor: "grey"}}>
+      <HomePage></HomePage>
+      <EmployeePage></EmployeePage>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/EmployeePage.jsx
@@ -0,0 +1,4 @@
+function EmployeePage(){
+    return <h1>Employee Page</h1>
+}
+export default EmployeePage;
